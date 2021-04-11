85789c0f602bde2d1aa0e250af19df9c7ca4a93f
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-11 11:11:23 -0400

    Added SeachBar.js and initial code to components folder. Added import and function call into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <wrapper>
       <Header />
+      <SearchBar />
     </wrapper>
   );
 }
