84ddab69850f446e20bc6d4ee027a0e6636613cc
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-11 10:42:43 -0400

    Created Header.js in components folder with Header function that displays Employee Directory heading. Added importo App.js file.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header className="header">
+            <h1>Employee Directory</h1>
+            <h5>Click on the carrots to filter by heading or use the search box to narrow results</h5>
+        </header>
+    );
+}
+
+export default Header;
