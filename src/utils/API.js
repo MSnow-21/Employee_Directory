83d5748a5587223fa46ccdc08b19c4a4fe4f2b45
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-14 15:24:44 -0400

    Updated API called results to US only.

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export default {
     getRandomUser: function() {
-        return axios.get("https://randomuser.me/api/?results=30");
+        return axios.get("https://randomuser.me/api/?results=30&nat=us");
     }
 };
